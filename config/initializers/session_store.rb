d30042066d567ff99c1d407abbb23da80ed98dd4
Yangzi Liu <asu.tristan@gmail.com>
2013-07-04 14:56:27 -0700

    init commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Odysseyonrails::Application.config.session_store :cookie_store, key: '_odysseyonrails_session'
+Odysseyonrails::Application.config.session_store :cookie_store, key: '_railsblogtemplate_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
